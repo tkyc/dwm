4a73870d8e4fef20353b43b6fd7d1fb7c9c9e0a3
tkyc <terrykyc.7000@gmail.com>
2026-01-05 22:54:11 -0800

    Changed to 'JetBrainsMono Nerd Font'

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const int showbar                   = 1; /* 0 means no bar */
 static const int topbar                    = 1; /* 0 means bottom bar */
-static const char *fonts[]                 = {"monospace:size=10"};
+static const char *fonts[]                 = {"JetBrainsMono Nerd Font Mono:style=Bold:size=16"};
 static const float rootcolor[]             = COLOR(0x000000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f}; /* You can also use glsl colors */
