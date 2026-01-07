1656745af135d64a0b1308fefe816672760bf3bc
tkyc <terrykyc.7000@gmail.com>
2026-01-06 18:47:14 -0800

    Removed versioning text in status bar

--- dwl.c
+++ dwl.c
@@ -3161,8 +3161,6 @@ updatemons(struct wl_listener *listener, void *data)
 		}
 	}
 
-	if (stext[0] == '\0')
-		strncpy(stext, "dwl-"VERSION, sizeof(stext));
 	wl_list_for_each(m, &mons, link) {
 		updatebar(m);
 		drawbar(m);
